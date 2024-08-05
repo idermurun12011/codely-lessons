73bd232b7816b97161604a6723f144d1f214e8e5
idermurun12011 <idre1201@gmail.com>
2024-08-05 09:15:29 +0800

    js

---
+++ 7sariin29 LESSON12/main.js
@@ -0,0 +1,64 @@
+// 1
+// console.log("Намайг" = 'idree' + name + " гэдэг. Би " + age +  " настай.")
+
+
+// 2
+// console.log("Hello", 'string'  )
+// console.log(true, 'boolean' )
+// console.log("Name", 'string'  )
+// console.log(33, 'number' )
+// console.log("90", 'number'  )
+// console.log(90+"77",  )
+// console.log( {name:'Anu', age:15}, 'object'  )
+// console.log(false, 'boolean' )
+// console.log(1655/5, 'array' )
+// console.log(["javascript", "html", "css"], 'array' )
+
+// 3  
+
+//  let a='cola';  
+//   let b='water';
+//   let c ;
+//   c=b;
+// b=a;
+// a=c;
+// c=null
+//   console.log( a,b,c ); 
+
+
+//  dasgal
+// let x = '2'
+// let y = '6'
+// let z = '3'
+// let a;
+// a=y
+// y=z
+// z=x
+// x=a
+// a=null
+// console.log(x,y,z,a);
+
+
+
+// 4    
+
+// let b = '6'
+// let c = '3'
+// let d = '2'
+// let a= b*c-d  ;
+// console.log(a); 
+
+
+// 5
+// let day= 2;
+// let time= 10;
+// let totalTime = day * 24 + time 
+// console.log(totalTime);
+
+
+// 6
+let futureYear = 2040
+let bornYear = 1999 
+
+let nas = futureYear - bornYear
+console.log(nas);
---
+++ 8sariin2 LESSON13/main.js
@@ -0,0 +1,49 @@
+// 1
+
+// if(56 > 78 ) {
+//     console.log(' 78 ni ih baina');
+// }else {
+//     console.log(' 56 ni baga baina');
+// }
+
+// 2
+
+let x = 57349
+if( x%2===0  ) {
+    console.log('tegsh');
+}else {
+    console.log('sondgoi');
+}
+   
+
+// 3
+
+let newPassword = 1234
+let confirmNewPassword = 1234
+if (newPassword === confirmNewPassword){
+    console.log('nuuts ug amjilttai soligdloo');
+} else {
+    console.log('nuuts ugee zuw dawtaj oruulaarai ');
+}
+
+// 4
+
+// let dun = 92
+// if ( dun >= 90 &&  dun <= 100 ) {
+//     console.log( 'A hangalttai sain' );
+// }else if (80) {
+//     console.log('B hangalttai');
+// } else if ( dun > 80 &&  dun < 90 ) {
+// console.log('c sain');
+// } 
+
+
+
+// 5
+// let a = 35 
+// if (  x%3===0 && x%5===0 ) {
+//  console.log('FizzBuzz');
+// }
+// else if
+
+
