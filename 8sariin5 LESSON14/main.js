ad9ab603893fbbf6e5d234e3fd362dcac091befe
idermurun12011 <idre1201@gmail.com>
2024-08-07 09:25:14 +0800

    js

---
+++ 8sariin5 LESSON14/main.js
@@ -0,0 +1,33 @@
+1
+let n = prompt()
+for (let i = 0; i < 5; i++) {
+   console.log( 'Codely %' +  i);
+   
+}
+
+
+// 2
+// let n = prompt()
+// for (let i = 5; i > 0; i--) {
+//    console.log( 'Codely %' +  i);
+   
+// }
+
+
+// 3
+// let niilber = 5
+// let n = prompt("Тоогоо оруулна уу")
+// let niilber = 0
+// for (let i = 1; i <= n; i++) {
+//     niilber = niilber + i;
+//    console.log( 'i=' +  i + ',niilber =' + niilber);
+//    alert(niilber)
+// }
+
+
+// 4
+
+// let n = prompt("Тоогоо оруулна уу")
+// for (let i = 0; i < 5; i++) {
+
+// }
